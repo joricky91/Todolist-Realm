72bd8fb72cf85942ae1e3211f3cfd2439ccfa720
joricky91 <jonathanricky91@gmail.com>
2023-02-24 18:44:04 +0700

    Initial Commit

---
+++ Todolist Realm/Todolist_RealmApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todolist_RealmApp.swift
+//  Todolist Realm
+//
+//  Created by Jonathan Ricky Sandjaja on 24/02/23.
+//
+
+import SwiftUI
+
+@main
+struct Todolist_RealmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
